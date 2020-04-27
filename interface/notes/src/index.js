1b3518ec0c5169378b1da848354abd9d852f46bd
John Franklin <jfranklin9000@gmail.com>
2020-04-27 05:30:32 -0500

    initial commit of (part of) interface/notes/src/

---
+++ interface/notes/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Root } from '/components/root';
+import { api } from '/api';
+// import { store } from '/store';
+import { subscription } from '/subscription';
+
+console.log("hello from notes");
+
+api.setAuthTokens({
+  ship: window.ship
+});
+
+subscription.start();
+
+ReactDOM.render((
+  <Root />
+), document.querySelectorAll("#root")[0]);
---
+++ interface/notes/src/js/store.js
@@ -0,0 +1,41 @@
+// import { InitialReducer } from '/reducers/initial';
+// import { ConfigReducer } from '/reducers/config';
+// import { UpdateReducer } from '/reducers/update';
+
+
+class Store {
+    constructor() {
+        // console.log('src/js/store.js constructor()');
+        this.state = {
+            source: '',    // udon
+            object: '',    // html
+            edited: false  // source and object are consistent
+        };
+
+        // this.initialReducer = new InitialReducer();
+        // this.configReducer = new ConfigReducer();
+        // this.updateReducer = new UpdateReducer();
+        this.setState = () => { };
+    }
+
+    setStateHandler(setState) {
+        // console.log('src/js/store.js setStateHandler()', setState);
+        this.setState = setState;
+    }
+
+    handleEvent(data) {
+        // console.log('src/js/store.js handleEvent()', data);
+        let json = data.data;
+
+        // this.initialReducer.reduce(json, this.state);
+        // this.configReducer.reduce(json, this.state);
+        // this.updateReducer.reduce(json, this.state);
+        this.state.source = json.state.source;
+        this.state.object = json.state.object;
+        this.state.edited = false;
+        this.setState(this.state);
+    }
+}
+
+export let store = new Store();
+window.store = store;
