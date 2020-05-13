0014382bcc2135d89de32681185643db21c299e9
John Franklin <jfranklin9000@gmail.com>
2020-05-13 01:42:09 -0500

    add New Note button and ~/notes/note page

--- interface/notes/src/js/components/lib/notes-ui.js
+++ interface/notes/src/js/components/lib/notes-ui.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
-export class KeywordSearchBar extends Component {
+export class KeywordsSearchNewNote extends Component {
   render() {
     const props = this.props;
-    const divC = 'flex flex-row justify-center items-baseline f5';
-    const spanC = 'mr3 gray3';
-    const inputC = 'w-50 bg-gray4 black tc';
-    const buttonC = 'ml3 ba b--green2 green2 br1 pointer';
+    // these are good for centered display without New Note button:
+    // const divC = 'flex flex-row justify-center items-baseline f5';
+    // const spanC = 'mr3 gray3';
+    // const inputC = 'w-50 bg-gray4 black tc';
+    // const buttonC = 'ml3 ba b--green2 green2 br1 pointer';
+    // instead we will put the New Note button here:
+    // (no flex here..)
+    const divC = 'f5';
+    const spanC = 'fl mr3 gray3 pt1';
+    const inputC = 'fl w-50 bg-gray4 black pt1 pb1 pl2 pr2';
+    const searchC = 'fl ml3 ba b--green2 green2 br1 pointer pt1';
+    const newNoteC = 'fr ml3 ba b--green2 green2 br1 pointer pt1';
     return (
       <div className={divC}>
         <span className={spanC}>
@@ -15,10 +23,15 @@ export class KeywordSearchBar extends Component {
         <input className={inputC}
           onChange={props.keysInput.bind(this)}
         />
-        <button className={buttonC}
+        <button className={searchC}
           onClick={props.searchButton.bind(this)}
         >
           Search
+        </button>
+        <button className={newNoteC}
+          onClick={props.newNoteButton.bind(this)}
+        >
+          New Note
         </button>
       </div>
     );
@@ -81,6 +94,40 @@ export class SearchResults extends Component {
         <Matches
           matches={search.matches}
         />
+      </div>
+    );
+  }
+}
+
+export class KeywordsSaveGoToSearch extends Component {
+  render() {
+    const props = this.props;
+    const divC = 'f5';
+    const spanC = 'fl mr3 gray3 pt1';
+    const inputC = 'fl w-50 bg-gray4 black pt1 pb1 pl2 pr2';
+// this should be gray if up-to-date XX
+    const saveC = 'fr ml3 ba b--green2 green2 br1 pointer pt1';
+// this could be yellow if not up-to-date XX
+    const goToSearchC = 'fr ml3 ba b--green2 green2 br1 pointer pt1';
+    return (
+      <div className={divC}>
+        <span className={spanC}>
+          Keywords
+        </span>
+        <input className={inputC}
+          onChange={props.keysInput.bind(this)}
+        />
+        {/* swap button order because they're float:right */}
+        <button className={goToSearchC}
+          onClick={props.goToSearchButton.bind(this)}
+        >
+          Go To Search
+        </button>
+        <button className={saveC}
+          onClick={props.saveButton.bind(this)}
+        >
+          Save
+        </button>
       </div>
     );
   }
--- interface/notes/src/js/components/root.js
+++ interface/notes/src/js/components/root.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { HeaderBar } from './lib/header-bar.js'
-import { KeywordSearchBar, SearchResults } from './lib/notes-ui.js'
+import { KeywordsSearchNewNote, SearchResults } from './lib/notes-ui.js'
+import { KeywordsSaveGoToSearch } from './lib/notes-ui.js'
 
 export class Root extends Component {
 
@@ -14,62 +15,109 @@ export class Root extends Component {
 
   keysInput(event) {
     // console.log('Root keysInput()', event.target.value);
-    this.setState({ keys: event.target.value });
+    this.setState({keys: event.target.value});
   }
 
   searchButton() {
     // console.log('Root searchButton()', this.state);
     api.action('notes', 'json',
-      { action: 'search', keys: this.state.keys });
+      {action: 'search', keys: this.state.keys});
+  }
+
+  newNoteButton() {
+    // console.log('Root newNoteButton()', this.state);
+    api.action('notes', 'json',
+      {action: 'new-note'});
+
+// need a redirect here.. /~notes/note/42 (but we don't know 42..)
+    // need a redirect to /~notes/note
+    // could not get this to work XX
+    // return <Redirect to='/~notes/note' />;
+    // yeesh, probably not right, but it works
+    window.location.replace('/~notes/note');
+  }
+
+  saveButton() {
+    // console.log('Root saveButton()', this.state);
+
+console.log('HELLO from saveButton()');
+  }
+
+  goToSearchButton() {
+    // console.log('Root goToSearchButton()', this.state);
+
+console.log('HELLO from goToSearchButton()');
   }
 
   textTextArea() {
     // console.log('Root textTextArea()', event.target.value);
-    this.setState({ text: event.target.value });
+    this.setState({text: event.target.value});
   }
 
   addButton() {
     // console.log('Root addButton()', this.state);
     api.action('notes', 'json',
-      { action: 'add', id: 0, keys: this.state.keys, text: this.state.text });
+      {action: 'add', id: 0, keys: this.state.keys, text: this.state.text});
 
     // need a redirect to /~notes/edit (/~notes for now)
     // could not get this to work XX
     // return <Redirect to='/~notes' />;
     // yeesh, probably not right, but it works
-    window.location.replace("/~notes");
+    window.location.replace('/~notes');
   }
 
   render() {
-    const { props, state } = this; // need props?
-
-    console.log('Root render()', state, props);
-
-    // need a New Note button on main page XX
+    const state = this.state;
+    console.log('Root render()', state);
 
     return (
       <BrowserRouter>
-        <div className="absolute h-100 w-100 bg-gray0-d ph4-m ph4-l ph4-xl pb4-m pb4-l pb4-xl">
+        <div className='absolute h-100 w-100 bg-gray0-d ph4-m ph4-l ph4-xl pb4-m pb4-l pb4-xl'>
           <HeaderBar/>
-          <Route exact path="/~notes" render={ () => {
+          <Route exact path='/~notes' render={ () => {
             return (
-              <div className="cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden">
-                <KeywordSearchBar
-                  keysInput={ this.keysInput }
-                  searchButton={ this.searchButton }
+              <div className='cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden'>
+                <KeywordsSearchNewNote
+                  keysInput={this.keysInput}
+                  searchButton={this.searchButton}
+                  newNoteButton={this.newNoteButton}
                 />
                 <SearchResults
-                  search={ state.search }
+                  search={state.search}
                 />
               </div>
             )}}
           />
 
-          <Route exact path="/~notes/add" render={ () => {
-            return (
-              <div className="cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden">
+          <Route exact path='/~notes/note/:id?' render={ props => {
+console.log(props.match.params); // get id from here, it may be undefined
 
-                <p style={{ fontSize: 1.0 + 'rem', color: 'lightgray' }}>
+            return (
+              <div className='cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden'>
+                <KeywordsSaveGoToSearch
+                  keysInput={this.keysInput}
+                  saveButton={this.saveButton}
+                  goToSearchButton={this.goToSearchButton}
+                />
+
+                {/* do we want to set value here? */}
+                <textarea
+                  className='pa2 pre f6 mt3 ba b--gray4 lh-copy overflow-auto'
+                  style={{flexBasis: '100%', resize: 'none'}}
+                  value={state.text}
+                  onChange={this.textTextArea.bind(this)}
+                >
+                </textarea>
+
+              </div>
+            )}}
+          />
+
+          <Route exact path='/~notes/add' render={ () => {
+            return (
+              <div className='cf w-100 flex flex-column pa4 ba-m ba-l ba-xl b--gray2 br1 h-100 h-100-minus-40-s h-100-minus-40-m h-100-minus-40-l h-100-minus-40-xl f9 white-d overflow-x-hidden'>
+
+                <p style={{fontSize: 1.0 + 'rem', color: 'lightgray'}}>
                   Add a note
                 </p>
 
@@ -82,14 +130,14 @@ export class Root extends Component {
                 />
 
                 {/* do we want to set value here? */}
-                <textarea className="br pa2 pre"
+                <textarea className='br pa2 pre'
                           style={{ flexBasis: '50%', resize: 'none' }}
                           value={ state.text }
                           onChange={ this.textTextArea.bind(this) }
                 >
                 </textarea>
 
-                <button className="mt3 fr f4"
+                <button className='mt3 fr f4'
                         style={{ color: 'lightgray', cursor: 'pointer' }}
                         onClick={ this.addButton.bind(this) }>
                   Add
