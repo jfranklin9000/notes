345f145018e76661027010a9adfb307ad52da9a2
John Franklin <jfranklin9000@gmail.com>
2020-05-14 23:27:57 -0500

    better search results styling

--- interface/notes/src/js/components/lib/notes-ui.js
+++ interface/notes/src/js/components/lib/notes-ui.js
@@ -57,16 +57,19 @@ class KeysNotIn extends Component {
     if (keys == null || keys.length == 0)
       return null;
     const message = keys.length == 1
-      ? 'This keyword matched no notes'
-      : 'These keywords matched no notes';
-    const _span = 'f6 red2 ml1 mr1';
-    const spans = keys.map((key, n) => {
-      return <span key={n} className={_span}>{key}</span>;
+      ? 'This keyword didn\'t match any notes'
+      : 'These keywords didn\'t match any notes';
+    const ks = keys.map((key, n) => {
+      return (
+        <span key={n} className={'f6 red2 ml1 mr1'}>
+          {key}
+        </span>
+      );
     });
     return (
       <div className={'mt3'}>
         <p className={'f8 tc'}>{message}</p>
-        <div className={'mt1 tc'}>{spans}</div>
+        <div className={'mt1 tc'}>{ks}</div>
       </div>
     );
   }
@@ -75,14 +78,29 @@ class KeysNotIn extends Component {
 // export
 class Matches extends Component {
   render() {
-    const matches = this.props.matches.map((match, n) => {
+    const matches = this.props.matches;
+    if (matches == null || matches.length == 0)
+      return (
+        <p className={'f7 gray3 tc mt7 i'}>
+          no matches
+        </p>
+      );
+    const ms = matches.map((match, n) => {
       // do something with match.keys FIXME
       let notes = match.notes.map((note, n) => {
-        return <p key={n}>{note.text}</p>;
+        return (
+          <p key={n} className={'f7 bt b--gray4 mt2 pt2'}>
+            {note.text}
+          </p>
+        );
       });
-      return <div key={n}>{notes}</div>
+      return (
+        <div key={n}>{notes}</div>
+      );
     });
-    return <div>{matches}</div>;
+    return (
+      <div className={'bb b--gray4 pb2'}>{ms}</div>
+    );
   }
 }
 
@@ -91,12 +109,8 @@ export class SearchResults extends Component {
     const search = this.props.search;
     return (
       <div>
-        <KeysNotIn
-          keysNi={search.keysNi}
-        />
-        <Matches
-          matches={search.matches}
-        />
+        <KeysNotIn keysNi={search.keysNi} />
+        <Matches matches={search.matches} />
       </div>
     );
   }
@@ -105,9 +119,6 @@ export class SearchResults extends Component {
 export class KeywordsSaveGoToSearch extends Component {
   render() {
     const props = this.props;
-    const divC = 'f6';
-    const spanC = 'fl mr3 gray3 pt1';
-    const inputC = 'fl w-50 bg-gray4 black pt1 pb1 pl2 pr2';
 // this should be gray if up-to-date XX
 //  const saveC = 'fr ml3 ba b--green2 green2 br1 pointer pt1';
     let saveC = 'fr ml3 ba br1 pt1';
@@ -123,11 +134,11 @@ export class KeywordsSaveGoToSearch extends Component {
       goToSearchC += ' b-green2 green2';
     }
     return (
-      <div className={divC}>
-        <span className={spanC}>
+      <div className={'f6'}>
+        <span className={'fl mr3 gray3 pt1'}>
           Keywords
         </span>
-        <input className={inputC}
+        <input className={'fl w-50 bg-gray4 black pt1 pb1 pl2 pr2'}
           onChange={props.keysInput.bind(this)}
         />
         {/* swap button order because they're float:right */}
